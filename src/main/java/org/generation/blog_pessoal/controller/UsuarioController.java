fb463de8ed011dfb3e2490e7e73d26f1e44cf074
Fernando Porto <fernandogomes143@gmail.com>
2022-03-10 13:30:31 -0300

    @fernandoportodev Task: Test e deploy da aplicação

--- src/main/java/org/generation/blog_pessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blog_pessoal/controller/UsuarioController.java
@@ -14,6 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -22,11 +28,39 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Operation(summary = "Logar usuario")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Usuario Logado",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UserLogin.class))}
+        ),
+
+        @ApiResponse(
+            responseCode = "401",
+            description = "Usuario não autorizado",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UserLogin.class))}
+        )
+    })
     @PostMapping("/logar")
     public ResponseEntity<UserLogin> Autentication (@RequestBody Optional<UserLogin> user){
         return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
+    @Operation(summary = "Cadastrar usuario")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "201",
+            description = "Usuario Criado",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Usuario.class))}
+        ),
+
+        @ApiResponse(
+            responseCode = "422",
+            description = "Usuario já cadastrado",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Usuario.class))}
+        )
+    })
     @PostMapping("/cadastrar")
     public ResponseEntity<Usuario> Post (@RequestBody Usuario usuario){
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.CadastrarUsuario(usuario));
